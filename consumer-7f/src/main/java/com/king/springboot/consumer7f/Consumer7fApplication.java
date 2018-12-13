76f56a59d7c33fb28c3c5332244c1f31dd3c5527
kingweight <31714938+kingweight@users.noreply.github.com>
2018-12-13 23:58:08 +0800

    init

--- consumer-7f/src/main/java/com/king/springboot/consumer7f/Consumer7fApplication.java
+++ consumer-7f/src/main/java/com/king/springboot/consumer7f/Consumer7fApplication.java
@@ -2,8 +2,10 @@ package com.king.springboot.consumer7f;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Consumer7fApplication {
 
 	public static void main(String[] args) {
--- provider-7f/src/main/java/com/king/springboot/provider7f/Provider7fApplication.java
+++ provider-7f/src/main/java/com/king/springboot/provider7f/Provider7fApplication.java
@@ -2,8 +2,10 @@ package com.king.springboot.provider7f;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Provider7fApplication {
 
 	public static void main(String[] args) {
